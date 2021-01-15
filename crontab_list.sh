40e3251df87162d51cc26be78767d32fef981872
AnthonyMSen <1072519956@qq.com>
2021-01-15 14:21:57 +0800

    Update crontab_list.sh

--- crontab_list.sh
+++ crontab_list.sh
@@ -1,5 +1,5 @@
 #每12小时15分钟时更新IYUU
-15 */12 * * * cd /IYUU && git fetch --all && git reset --hard origin/master >> /logs/update_iyuu.log 2>&1
+15 */12 * * * git -C /IYUU pull >> /logs/update_iyuu.log 2>&1
 
 #每小时45分运行IYUU
 45 * * * * php /IYUU/iyuu.php >> /logs/run_iyuu.log 2>&1
