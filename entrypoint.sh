e8d741ea28f2beb4ca1588b607a64384cfc5811a
AnthonyMSen <1072519956@qq.com>
2021-01-15 13:40:19 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+export LANG="zh_CN.UTF-8"
+
+if [ ! -e /IYUU/iyuu.php ] 
+then
+        echo "容器启动，未发现代码，正在clone中..."
+        git clone https://gitee.com/ledc/IYUUAutoReseed.git /IYUU
+fi
+
+echo "Container start , Pull the latest code..."
+echo "容器启动，git 拉取最新代码..."
+git -C /IYUU pull
+
+
+
+ListFile="/$CRONTAB_LIST"
+
+echo "Load the latest crontab task file..."
+echo "加载最新的定时任务文件..."
+crontab $ListFile
+
+echo "Start crontab task main process..."
+echo "启动crondtab定时任务主进程..."
+crond -f
